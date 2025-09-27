ecf2bb45076286cedbf9ceb155d872eb566a1b1c
Seung Ki Lee <53922526+seungkilee-cs@users.noreply.github.com>
2025-09-28 08:07:20 +0900

    "Setup: Initial Kotlin Spring Boot backend" (#1)
    * backend Kotlin Spring Boot Setup and setup instructions added, .gitignore updated for the root and backend dir for monorepo handling
    
    * fixed image on readme and added .github directory with templates
    
    * readme clarification
    
    * cleanup and template setup

---
+++ backend/src/test/kotlin/com/histoflow/backend/HistoFlowBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.histoflow.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HistoFlowBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
